dde66caa1b59e64c8cd2aefda395825a86b97576
Pushkar Arun <arunpushkar0120@gmail.com>
2026-04-17 15:04:24 +0530

    Update Manashvi.java

--- Manashvi.java
+++ Manashvi.java
@@ -1,1 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class UseCaseITrainConsistMgmnt {
+
+    public static void main(String[] args) {
+
+        System.out.println("  === Train Consist Management App === ");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train initialized successfully...");
+        System.out.println("Initial Bogie Count : " + trainConsist.size());
+        System.out.println("Current Train Consist : " + trainConsist);
+        System.out.println("\nSystem ready for operations...");
+    }
+}
